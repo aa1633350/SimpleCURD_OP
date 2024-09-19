79b73529c3d65ac58d8e840a876094b2ed60579f
Aditya Anand <adanand@twilio.com>
2024-09-19 19:15:06 +0530

    Initial Commit, added basic functions to perform CURD ops

---
+++ go.mod
@@ -0,0 +1,1 @@
+module SimpleCURD_OP
---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	ID      int    `json:"id"`
+	Name    string `json:"name"`
+	Mobile  string `json:"mobile number"`
+	Address string `json:"address"`
+	DOB     string `json:"DOB"`
+}
+
+var people = make(map[int]Person)
+var currentId = 1
+
+func CreatePerson(p Person) Person {
+	p.ID = currentId
+	people[currentId] = p
+	currentId++
+	return p
+}
+
+func GetPerson(id int) Person {
+	person, exists := people[id]
+	if !exists {
+		fmt.Printf("person with id %T does not exist", id)
+	}
+	return person
+}
+
+func UpdatePerson(id int, update Person) (Person, bool) {
+	if person, exists := people[id]; exists {
+		person.Name = update.Name
+		person.DOB = update.DOB
+		person.Mobile = update.Mobile
+		person.Address = update.Address
+		people[id] = person
+		return person, true
+	}
+	return Person{}, false
+}
+
+// DeletePerson deletes a person from the in-memory map.
+func DeletePerson(id int) bool {
+	if _, exists := people[id]; exists {
+		delete(people, id)
+		return true
+	}
+	return false
+}
+
+func main() {
+
+}
