3c3b3195396730d4db33685a843be252bf54bd15
Aditya Anand <aa1633350@gmail.com>
2024-09-27 13:37:24 +0530

    added function to test api endpoint with goroutines

--- goroutines/demo_goroutine.go
+++ goroutines/demo_goroutine.go
@@ -4,10 +4,11 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"sync"
 	"time"
 )
 
-const apikey = "4701a62b0df8e6a51830dc261cb2eaf5"
+const apikey = "xx01a62b0df8e6a51830dc261cbxxxxx"
 
 // code without using go routine, will add go routine in next commit
 // Sample output :
@@ -17,13 +18,45 @@ const apikey = "4701a62b0df8e6a51830dc261cb2eaf5"
 // This is the data for: Patna {Main:{Temp:301.11}}
 // This operation took :  559.819125ms
 
-func fetchWeatherData(city string) interface{} {
+//func fetchWeatherData(city string) interface{} {
+//
+//	var data struct {
+//		Main struct {
+//			Temp float64 `json:"temp"`
+//		} `json:"main"`
+//	}
+//
+//	url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s", city, apikey)
+//	res, err := http.Get(url)
+//	if err != nil {
+//		fmt.Printf("Error fetching data for city %s: %s ", city, err)
+//		return data
+//	}
+//	defer res.Body.Close()
+//	if err := json.NewDecoder(res.Body).Decode(&data); err != nil {
+//		fmt.Printf("Error decoding data for city %s: %s ", city, err)
+//		return data
+//	}
+//
+//	return data
+//}
+
+// Demo run with go routines
+//This is the city Patna
+//This is the city Delhi
+//This is the city Banglore
+//This is the city Mumbai
+//This operation took :  157.35ms
+
+func fetchWeatherData(city string, ch chan<- string, wg *sync.WaitGroup) interface{} {
 
 	var data struct {
 		Main struct {
 			Temp float64 `json:"temp"`
 		} `json:"main"`
 	}
+
+	defer wg.Done()
 
 	url := fmt.Sprintf("https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s", city, apikey)
 	res, err := http.Get(url)
@@ -36,17 +69,30 @@ func fetchWeatherData(city string) interface{} {
 		fmt.Printf("Error decoding data for city %s: %s ", city, err)
 		return data
 	}
+	ch <- fmt.Sprintf("This is the city %s", city)
 
 	return data
 }
 
 func main() {
+
 	startNow := time.Now()
 	cities := []string{"Delhi", "Banglore", "Mumbai", "Patna"}
+	ch := make(chan string)
+	var wg sync.WaitGroup
 	for _, city := range cities {
-		data := fetchWeatherData(city)
-		fmt.Println("This is the data ", data)
+		wg.Add(1)
+		go fetchWeatherData(city, ch, &wg)
 
+	}
+	// asking the goroutine to wait before quitting
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	for result := range ch {
+		fmt.Println(result)
 	}
 	fmt.Println("This operation took : ", time.Since(startNow))
 }
